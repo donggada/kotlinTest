4e8b0a4fc037d52a310ef582c689c1c16cf777a9
donggada <koem0425@naver.com>
2024-04-12 17:36:57 +0900

    백준 완전탐색 풀이

---
+++ src/test/kotlin/com/example/kotlinTest/백준_완전탐색/Test15686.java
@@ -0,0 +1,17 @@
+package com.example.kotlinTest.백준_완전탐색;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Test15686 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] info = br.readLine().split(" ");
+        int N = Integer.parseInt(info[0]);
+        int M = Integer.parseInt(info[1]);
+
+        
+    }
+}
