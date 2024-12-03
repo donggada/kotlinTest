a6c4048de416ee446fdf93528a9889d86b77b406
donggada <koem0425@naver.com>
2024-12-03 15:55:47 +0900

    Update Main11726.java

--- src/test/kotlin/com/example/kotlinTest/백준_DP/Main11726.java
+++ src/test/kotlin/com/example/kotlinTest/백준_DP/Main11726.java
@@ -17,10 +17,13 @@ public class Main11726 {
         int[] dp = new int[n+1];
         dp[0] = 0;
         dp[1] = 1;
-        dp[2] = 2;
+
+        if (n < 1) {
+            dp[2] = 2;
+        }
 
         for (int i = 3; i < n+1 ; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
         }
 
         System.out.println(dp[n]);
