daeb003b890bbe51b14f4575880c4275062bb36f
donggada <koem0425@naver.com>
2024-12-08 17:51:03 +0900

    코드 리펙토링

--- src/test/kotlin/com/example/kotlinTest/백준_그리디/Main1092.java
+++ src/test/kotlin/com/example/kotlinTest/백준_그리디/Main1092.java
@@ -10,7 +10,7 @@ public class Main1092 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Integer n = Integer.valueOf(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         List<Integer> crane = new ArrayList<>();
         List<Integer> box = new ArrayList<>();
 
@@ -19,15 +19,15 @@ public class Main1092 {
             crane.add(Integer.valueOf(s[i]));
         }
 
-        Integer m = Integer.valueOf(br.readLine());
+        int m = Integer.parseInt(br.readLine());
         String[] sp = br.readLine().split(" ");
 
         for (int i = 0; i < m; i++) {
             box.add(Integer.valueOf(sp[i]));
         }
 
-        Collections.sort(crane, Comparator.reverseOrder());
-        Collections.sort(box, Comparator.reverseOrder());
+        crane.sort(Comparator.reverseOrder());
+        box.sort(Comparator.reverseOrder());
 
         if(crane.get(0) < box.get(0)) {
             System.out.println(-1);
@@ -38,12 +38,12 @@ public class Main1092 {
 
         while (!box.isEmpty()) {
             int index = 0;
-            for(int i = 0; i < crane.size();) {
-                if(index == box.size()) {
+            for (int i = 0; i < crane.size();) {
+                if (index == box.size()) {
                     break;
                 }
 
-                if(crane.get(i) >= box.get(index)) {
+                if (crane.get(i) >= box.get(index)) {
                     box.remove(index);
                     i++;
                     continue;
