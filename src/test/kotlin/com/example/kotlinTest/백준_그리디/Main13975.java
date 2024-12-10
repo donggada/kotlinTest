46a0228da510423234b1d2341c899ef894aaad1b
donggada <koem0425@naver.com>
2024-12-10 16:07:52 +0900

    백준 [13975 - 파일합치기3] 문제풀이
    https://www.acmicpc.net/problem/13975
    풀이 알고리즘 그리디 알고리즘

---
+++ src/test/kotlin/com/example/kotlinTest/백준_그리디/Main13975.java
@@ -0,0 +1,36 @@
+package com.example.kotlinTest.백준_그리디;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main13975 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t ; i++) {
+            int n = Integer.parseInt(br.readLine());
+            PriorityQueue<Long> queue = new PriorityQueue<>();
+            String[] s = br.readLine().split(" ");
+            for (int j = 0; j < n ; j++) {
+                queue.add(Long.parseLong(s[j]));
+            }
+            long max = 0;
+
+            while (!queue.isEmpty()) {
+                Long poll = queue.poll();
+                if (!queue.isEmpty()) {
+                    long sum = poll + queue.poll();
+                    max += sum;
+                    queue.add(sum);
+                }
+            }
+
+            System.out.println(max);
+        }
+    }
+}
