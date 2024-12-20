a044894ff668322245c02f75ecdbe4383e6abca6
donggada <koem0425@naver.com>
2024-12-21 03:22:41 +0900

    백준 [문자열 폭발 - 9935] 문제풀이
    https://www.acmicpc.net/problem/9935
    풀이 알고리즘 - 문자열 + stack

---
+++ src/test/kotlin/com/example/kotlinTest/백준_문자열/Main9935.java
@@ -0,0 +1,41 @@
+package com.example.kotlinTest.백준_문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
+public class Main9935 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        String bomb = br.readLine();
+        int length = bomb.length();
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length() ; i++) {
+            stack.add(s.charAt(i));
+
+            if (stack.size() >= length) {
+                int count = 0;
+                for (int j = 0; j < length ; j++) {
+                    if (stack.get(stack.size() - length + j) == bomb.charAt(j)) {
+                        count++;
+                    }
+
+                    if (count == length) {
+                        for (int k = 0; k < length ; k++) {
+                            stack.pop();
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(stack.isEmpty() ? "FRULA" : stack.stream().map(String::valueOf).collect(Collectors.joining()));
+
+    }
+}
