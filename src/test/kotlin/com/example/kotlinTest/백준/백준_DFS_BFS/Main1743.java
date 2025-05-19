f3556575ec1bb39ad2170ab05880107a239656cb
donggada <koem0425@naver.com>
2025-05-19 17:32:02 +0900

    문제풀이

--- src/test/kotlin/com/example/kotlinTest/백준/백준_DFS_BFS/Main1743.java
+++ src/test/kotlin/com/example/kotlinTest/백준/백준_DFS_BFS/Main1743.java
@@ -23,15 +23,25 @@ public class Main1743 {
             String[] sp = br.readLine().split(" ");
             map[Integer.parseInt(sp[0])-1][Integer.parseInt(sp[1])-1] = true;
         }
+        int result = 0;
+        for (int i = 0; i < n ; i++) {
+            for (int j = 0; j < m ; j++) {
+                if (map[i][j] && !visited[i][j]) {
+                    result = Math.max(result , bfs(new int[]{i,j}, map, visited));
+                }
+            }
+        }
 
+        System.out.println(result);
     }
 
-    static private void bfs(boolean[][] map, boolean[][] visited) {
+    static private int bfs(int[] move, boolean[][] map, boolean[][] visited) {
         LinkedList<int[]> queue = new LinkedList<>();
-
+        int count = 1;
+        visited[move[0]][move[1]] = true;
+        queue.add(move);
         while (!queue.isEmpty()) {
             int[] pop = queue.pop();
-
 
             for (int i = 0; i < 4 ; i++) {
                 int nx = pop[0] + dx[i];
@@ -41,12 +51,13 @@ public class Main1743 {
                 if(nx > -1 && nx < map.length && ny > -1 && ny < map[0].length) {
                     if (!visited[nx][ny] && map[nx][ny]) {
                         queue.add(new int[]{nx,ny});
+                        visited[nx][ny] = true;
+                        count++;
                     }
                 }
             }
         }
+
+        return count;
     }
-
-
-
 }
